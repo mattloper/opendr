495f1fd523d9374ebf750951a435ff231ea59304
Matthew Loper <matt.loper@gmail.com>
2014-02-16 13:09:00 +0100

    test_dr/test_depth_renderer.py: updated with derivative testing

--- test_dr/test_depth_renderer.py
+++ test_dr/test_depth_renderer.py
@@ -7,7 +7,7 @@ visualize = False
 
 class TestDepthRenderer(unittest.TestCase):
 
-    def test_depth_renderer(self):
+    def test_depth_image(self):
         # Create renderer
         import chumpy as ch
         from opendr.renderer import DepthRenderer
@@ -30,17 +30,120 @@ class TestDepthRenderer(unittest.TestCase):
         # print 'took %es' % (time.time() - tm)
 
         # Show it
-        if visualize:
-            import matplotlib.pyplot as plt
-            plt.ion()
-            plt.imshow(rn.r)
-            plt.show()
+        # if visualize:
+        #     import matplotlib.pyplot as plt
+        #     plt.ion()
+        #     plt.imshow(rn.r)
+        #     plt.show()
 
         # print np.min(rn.r.ravel())
         # print np.max(rn.r.ravel())
         self.assertTrue(np.abs(np.min(rn.r.ravel()) - 3.98) < 1e-5)
         self.assertTrue(np.abs(np.min(m.v[:,2]) - np.min(rn.r.ravel())) < 1e-5)
         self.assertTrue(np.abs(rn.r[h/2,w/2] - 3.98) < 1e-5)
+
+    def test_derivatives(self):
+        import chumpy as ch
+        from chumpy.utils import row
+        import numpy as np
+        from opendr.renderer import DepthRenderer
+
+        rn = DepthRenderer()
+
+        # Assign attributes to renderer
+        from opendr.test_dr.common import get_earthmesh
+        m = get_earthmesh(trans=ch.array([0,0,4]), rotation=ch.zeros(3))
+        w, h = (320, 240)
+        from opendr.camera import ProjectPoints
+        rn.camera = ProjectPoints(v=m.v, rt=ch.zeros(3), t=ch.zeros(3), f=ch.array([w,w])/2., c=ch.array([w,h])/2., k=ch.zeros(5))
+        rn.frustum = {'near': 1., 'far': 10., 'width': w, 'height': h}
+        rn.set(v=m.v, f=m.f, bgcolor=ch.zeros(3))
+
+        if visualize:
+            import matplotlib.pyplot as plt
+            plt.figure()
+        for which in range(3):
+            r1 = rn.r
+
+            adder = np.zeros(3)
+            adder[which] = .01
+            change = rn.v.r * 0 + row(adder)
+            dr_pred = rn.dr_wrt(rn.v).dot(change.ravel()).reshape(rn.shape)
+            rn.v = rn.v.r + change
+
+            r2 = rn.r
+            dr_emp = r2 - r1
+
+            # print np.mean(np.abs(dr_pred-dr_emp))
+
+            self.assertTrue(np.mean(np.abs(dr_pred-dr_emp)) < .025)
+
+            if visualize:
+                plt.subplot(2,3,which+1)
+                plt.imshow(dr_pred)
+                plt.clim(-.01,.01)
+                plt.title('emp')
+                plt.subplot(2,3,which+4)
+                plt.imshow(dr_emp)
+                plt.clim(-.01,.01)
+                plt.title('pred')
+                # cv2.imshow('pred%d' % (which,), dr_pred / np.max(dr_emp.ravel())*200.)
+                # cv2.imshow('emp%d' % (which,), dr_emp / np.max(dr_emp.ravel())*200.)
+
+    def test_derivatives2(self):
+        import chumpy as ch
+        from chumpy.utils import row
+        import numpy as np
+        from opendr.renderer import DepthRenderer
+
+        rn = DepthRenderer()
+
+        # Assign attributes to renderer
+        from opendr.test_dr.common import get_earthmesh
+        m = get_earthmesh(trans=ch.array([0,0,4]), rotation=ch.zeros(3))
+        w, h = (320, 240)
+        from opendr.camera import ProjectPoints
+        rn.camera = ProjectPoints(v=m.v, rt=ch.zeros(3), t=ch.zeros(3), f=ch.array([w,w])/2., c=ch.array([w,h])/2., k=ch.zeros(5))
+        rn.frustum = {'near': 1., 'far': 10., 'width': w, 'height': h}
+        rn.set(v=m.v, f=m.f, bgcolor=ch.zeros(3))
+
+        if visualize:
+            import matplotlib.pyplot as plt
+            plt.ion()
+            plt.figure()
+
+        for which in range(3):
+            r1 = rn.r
+
+            adder = np.random.rand(rn.v.r.size).reshape(rn.v.r.shape)*.01
+            change = rn.v.r * 0 + adder
+            dr_pred = rn.dr_wrt(rn.v).dot(change.ravel()).reshape(rn.shape)
+            rn.v = rn.v.r + change
+
+            r2 = rn.r
+            dr_emp = r2 - r1
+
+            print np.mean(np.abs(dr_pred-dr_emp))
+
+            self.assertTrue(np.mean(np.abs(dr_pred-dr_emp)) < .019)
+
+            if visualize:
+                plt.subplot(2,3,which+1)
+                plt.imshow(dr_pred)
+                plt.clim(-.01,.01)
+                plt.title('emp')
+                plt.subplot(2,3,which+4)
+                plt.imshow(dr_emp)
+                plt.clim(-.01,.01)
+                plt.title('pred')
+                plt.draw()
+                plt.show()
+                # cv2.imshow('pred%d' % (which,), dr_pred / np.max(dr_emp.ravel())*200.)
+                # cv2.imshow('emp%d' % (which,), dr_emp / np.max(dr_emp.ravel())*200.)
+            # if True: # visualize:
+            #     import cv2
+            #     cv2.imshow('pred%d' % (which,), dr_pred / np.max(dr_emp.ravel())*200.)
+            #     cv2.imshow('emp%d' % (which,), dr_emp / np.max(dr_emp.ravel())*200.)
 
 if __name__ == '__main__':
     visualize = True
