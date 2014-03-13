4840e2d75a67179fbc50e30c50f5941490be0153
Matthew Loper <matt.loper@gmail.com>
2014-03-13 02:14:33 +0100

    utils.py (mstack): new fn

--- utils.py
+++ utils.py
@@ -4,13 +4,23 @@ Author(s): Matthew Loper
 
 See LICENCE.txt for licensing and contact information.
 """
+__all__ = ['mstack', 'wget']
 
+def mstack(vs, fs):
+    import chumpy as ch
+    import numpy as np
+    lengths = [v.shape[0] for v in vs]
+    f = np.vstack([fs[i]+np.sum(lengths[:i]).astype(np.uint32) for i in range(len(fs))])
+    v = ch.vstack(vs)
 
-import urllib2
-from os.path import split, join
+    return v, f
+
 
 
 def wget(url, dest_fname=None):
+    import urllib2
+    from os.path import split, join
+
     curdir = split(__file__)[0]
     if dest_fname is None:
         dest_fname = join(curdir, split(url)[1])
