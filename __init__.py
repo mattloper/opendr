943aeaf6f093f892b8e2ff3da068a2cb00a4f730
Matthew Loper <matt.loper@gmail.com>
2014-06-18 13:04:03 +0200

    __init__.py: added 'optimization' demo

--- __init__.py
+++ __init__.py
@@ -191,8 +191,65 @@ plt.show()
 dr = rn.dr_wrt(rn.v) # or rn.vc, or rn.camera.rt, rn.camera.t, rn.camera.f, rn.camera.c, etc
 """
 
+demos['optimization'] = """
+from opendr.simple import *      
+import numpy as np 
+import matplotlib.pyplot as plt
+w, h = 320, 240                                                                 
+        
+try:
+    m = load_mesh('earth.obj')
+except:                                                             
+    from opendr.test_dr.common import get_earthmesh
+    m = get_earthmesh(trans=ch.array([0,0,4]), rotation=ch.zeros(3))                                             
+                                                                                
+# Create V, A, U, f: geometry, brightness, camera, renderer                     
+V = ch.array(m.v)                                                               
+A = SphericalHarmonics(vn=VertNormals(v=V, f=m.f),                              
+                       components=[3.,1.,0.,0.,0.,0.,0.,0.,0.],                 
+                       light_color=ch.ones(3))                                  
+U = ProjectPoints(v=V, f=[300,300.], c=[w/2.,h/2.], k=ch.zeros(5),              
+                  t=ch.zeros(3), rt=ch.zeros(3))                                
+f = TexturedRenderer(vc=A, camera=U, f=m.f, bgcolor=[0.,0.,0.],                 
+                     texture_image=m.texture_image, vt=m.vt, ft=m.ft,           
+                     frustum={'width':w, 'height':h, 'near':1,'far':20})     
+
+# Parameterize the vertices                                                     
+translation, rotation = ch.array([0,0,4]), ch.zeros(3)                          
+f.v = translation + V.dot(Rodrigues(rotation))                                  
+                                                                                
+try:
+    observed = load_image('earth_observed.jpg')
+except:
+    observed = f.r 
+    translation[:] = translation.r + np.random.rand(3)*.25
+    rotation[:] = rotation.r + np.random.rand(3)*.25
+
+# Create the energy
+difference = f - observed
+E = gaussian_pyramid(difference, n_levels=6, normalization='SSE')               
+
+plt.ion()
+global cb
+global difference
+global plt
+
+def cb(_):
+    plt.imshow(np.abs(difference.r))
+    plt.draw()
+         
+         
+# Minimize the energy                                                           
+light_parms = A.components     
+print 'OPTIMIZING TRANSLATION'                                                 
+ch.minimize({'energy': E}, x0=[translation], callback=lambda _ : cb(difference)) 
+print 'OPTIMIZING TRANSLATION, ROTATION, AND LIGHT PARMS'                                                 
+ch.minimize({'energy': E}, x0=[translation, rotation, light_parms], callback=cb) 
+"""
+
 
 def demo(which=None):
+    import re
     if which not in demos:
         print 'Please indicate which demo you want, as follows:'
         for key in demos:
@@ -200,6 +257,6 @@ def demo(which=None):
         return
 
     print '- - - - - - - - - - - <CODE> - - - - - - - - - - - -'
-    print demos[which]
+    print re.sub('global.*\n','',demos[which])
     print '- - - - - - - - - - - </CODE> - - - - - - - - - - - -\n'
     exec('global np\n' + demos[which], globals(), locals())
