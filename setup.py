6da6495fc4f405732a8054edd61c6bbe3ad4556a
Matthew Loper <matt.loper@gmail.com>
2014-07-26 18:23:48 -0400

    setup.py: added more information for pypi

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ def autogen_opengl_sources():
 def setup_opendr(ext_modules):
     ext_modules=cythonize(ext_modules)
     setup(name='opendr',
-            version='0.56',
+            version='0.57',
             packages = ['opendr', 'opendr.contexts', 'opendr.test_dr'],
             package_dir = {'opendr': '.'},
             author = 'Matthew Loper',
@@ -62,9 +62,35 @@ def setup_opendr(ext_modules):
             url = 'http://github.com/mattloper/opendr',
             ext_package='opendr',
             package_data={'opendr': ['test_dr/nasa*']},
-            install_requires=['cython', 'chumpy >= 0.51', 'matplotlib'],
+            install_requires=['cython', 'chumpy >= 0.53', 'matplotlib'],
             description='opendr',
             ext_modules=ext_modules,
+            license='MIT',
+
+            # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+            classifiers=[
+                # How mature is this project? Common values are
+                #   3 - Alpha
+                #   4 - Beta
+                #   5 - Production/Stable
+                'Development Status :: 4 - Beta',
+
+                # Indicate who your project is intended for
+                'Intended Audience :: Science/Research',
+                'Topic :: Multimedia :: Graphics :: 3D Rendering',                
+
+                # Pick your license as you wish (should match "license" above)
+                'License :: OSI Approved :: MIT License',
+
+                # Specify the Python versions you support here. In particular, ensure
+                # that you indicate whether you support Python 2, Python 3 or both.
+                'Programming Language :: Python :: 2',
+                'Programming Language :: Python :: 2.7',
+                
+                'Operating System :: MacOS :: MacOS X',
+                'Operating System :: POSIX :: Linux'
+            ],
+            
           )
 
 
